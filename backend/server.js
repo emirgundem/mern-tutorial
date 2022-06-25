e0e915176a8c0b50050658930a7300759bc954a4
emirgundem <emirgndem@gmail.com>
2022-06-25 21:13:23 +0300

    inital commit

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const colors = require('colors')
+dotenv.config()
+const app = express()
+
+
+
+
+
+
+const PORT = process.env.PORT || 5000
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on  ${PORT}`.cyan.green)
+})
